06fe00e57b00fdc217a97b5d4229083fb5909686
HerczegAdam <herczegadam@yahoo.com>
2016-01-25 16:36:33 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -43,14 +43,14 @@ class Test_Assignment_01(unittest.TestCase):
         """
         A test that will fail.
         """
-        self.assertTrue(True, 'This should fail, please fix it.')
+        self.assertTrue(True)
 
     def test_assert_equality(self):
         """
         A test for equality by assigning a value to a variable
         and evaluating an expression.
         """
-        expected_value = _
+        expected_value = 2
         truth_value = 1 + 1
         self.assertEqual(expected_value, truth_value)
 
@@ -58,7 +58,7 @@ class Test_Assignment_01(unittest.TestCase):
         """
         A test to know what the types of the previous fixes were
         """
-        self.assertFalse(True, bool)
+        self.assertFalse(False, bool)
 
     def test_assert_string(self):
         """
@@ -66,14 +66,14 @@ class Test_Assignment_01(unittest.TestCase):
         """
         my_string = 'Hello World'
         my_string_length = len(my_string)  # The expression
-        self.assertEqual(10, my_string_length)
+        self.assertEqual(11, my_string_length)
 
     def test_big_integers(self):
         """
         A test to explore notation of big integers.
         """
         x = 42,000
-        self.assertTrue(isinstance(x, int))
+        self.assertFalse(isinstance(x, int))
 
     def test_bigger_integers(self):
         """
@@ -81,7 +81,7 @@ class Test_Assignment_01(unittest.TestCase):
         """
         big = 1e6
         self.assertEqual(big, 100)
-        self.assertTrue(type(big), int)
+        self.assertFalse(type(big), int)
 
         small = 1e-5
         self.assertEqual(small, 0.0001)
@@ -95,7 +95,7 @@ class Test_Assignment_01(unittest.TestCase):
         self.assertTrue(type(i) == int)
         self.assertTrue(isinstance(i, float))  # These lines do the same type checking
         i = float(i)
-        self.assertTrue(isinstance(i, float))
+        self.assertFalse(isinstance(i, float))
         i = str(i)
         self.assertFalse(type(i) == str)
 
@@ -113,7 +113,7 @@ class Test_Assignment_01(unittest.TestCase):
         A test to show how rounding can get you
         """
         j = 3.9999
-        self.assertTrue(int(j), float)
+        self.assertFalse(int(j), float)
         self.assertEqual(int(j), 4)
 
     def tearDown(self):
